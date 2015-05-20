eecce1126813bf871ebbc8d7f07c8c9c0e45fbcb
marsim0 <marb@mail.ru>
2015-05-20 21:59:11 +0300

    Initial Commit

---
+++ thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thread
+//
+//  Created by Мариам Б. on 20.05.15.
+//  Copyright (c) 2015 Мариам Б. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
